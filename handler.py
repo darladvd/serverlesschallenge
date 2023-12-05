9bf2d0d0f57cb11e9a2629ba1099a392885e8564
darladvd <201910063@feualabang.edu.ph>
2023-12-05 18:09:35 +0800

    Added get_one_loyalty_card

--- handler.py
+++ handler.py
@@ -58,4 +58,39 @@ def get_all_loyalty_card(event, context):
 
 
 def get_one_loyalty_card(event, context):
-    pass
+    body = {
+        "message": "I'm getting a loyalty card",
+        "input": event,
+    }
+
+    try:
+        # Extract card number from the event or query parameters
+        card_number = event["headers"]["card_number"]
+
+        table_name = os.getenv("DYNAMODB_CARDS_TABLE_NAME")
+
+        # Query DynamoDB to get the loyalty card with the specified card number
+        result = DynamodbGateway.query_by_partition_key(
+            table_name=table_name,
+            partition_key_name="card_number",
+            partition_key_query_value=card_number
+        )
+
+        if result["items"]:
+            response = {
+                "statusCode": 200,
+                "body": json.dumps({"status": "success", "item": result["items"][0]})
+            }
+        else:
+            response = {
+                "statusCode": 404,
+                "body": json.dumps({"status": "error", "message": "Loyalty card not found"})
+            }
+
+    except Exception as e:
+        response = {
+            "statusCode": 500,
+            "body": json.dumps({"status": "error", "message": str(e)})
+        }
+
+    return response
