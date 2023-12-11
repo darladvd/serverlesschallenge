01d64fd2fab32ef81882666cd600fc24f2697e58
darladvd <201910063@feualabang.edu.ph>
2023-12-11 09:01:30 +0800

    SQS to Lambda to DynamoDB

--- handler.py
+++ handler.py
@@ -5,6 +5,7 @@ import os
 import boto3
 import urllib.parse
 import csv
+import sys
 from io import StringIO
 
 from dynamodb_gateway import DynamodbGateway
@@ -41,7 +42,7 @@ def create_loyalty_card(event, context):
 
             card = {
                 "card_number": id_var,
-                "customer_name": person.get("name"),
+                "first_name": person.get("name"),
                 "email": person.get("email")
             }
 
@@ -64,7 +65,7 @@ def create_loyalty_card(event, context):
 
         card = {
             "card_number": id_var,
-            "customer_name": body.get("name"),
+            "first_name": body.get("name"),
             "email": body.get("email")
         }
 
@@ -79,18 +80,6 @@ def create_loyalty_card(event, context):
     response = {"statusCode": 200, "body": json.dumps(loyalty_cards)}
 
     return response
-
-
-def email_exists(table_name, email):
-    # Check if the email already exists in the DynamoDB table using GSI
-    result = DynamodbGateway.query_index_by_partition_key(
-        index_name="emailIndex",
-        table_name=table_name,
-        partition_key_name="email",
-        partition_key_query_value=email
-    )
-
-    return bool(result)
     
 
 def get_all_loyalty_card(event, context):
@@ -155,58 +144,115 @@ def get_one_loyalty_card(event, context):
     return response
 
 #aws lambda trigger when theres new s3 file. reads line by line
-def prepare_sqs_job(event, content):
-    bucket_name = os.getenv("S3_BUCKETNAME")
-
-    # Get the object from the event and show its content type
-    bucket = event['Records'][0]['s3']['bucket']['name']
-    file_key = urllib.parse.unquote_plus(event['Records'][0]['s3']['object']['key'], encoding='utf-8')
+def prepare_sqs_job(event, context):
     try:
+        print(f"Received S3 event: {json.dumps(event)}")
+
+        bucket_name = os.getenv("S3_BUCKETNAME")
+
+        # Get the object details from the S3 event
+        s3_record = event['Records'][0]['s3']
+        bucket = s3_record['bucket']['name']
+        file_key = urllib.parse.unquote_plus(s3_record['object']['key'], encoding='utf-8')
+
         # Download the file from S3
         response = s3.get_object(Bucket=bucket, Key=file_key)
-        
-        # Read the file contents line by line
         file_content = response['Body'].read().decode('utf-8')
         print(f"Object uploaded: s3://{bucket}/{file_key}")
 
-        rows = []
-        csv_reader = csv.reader(StringIO(file_content))
+        # Process CSV file and send each row as a message to SQS
+        rows = [row for i, row in enumerate(csv.reader(StringIO(file_content))) if i > 0]
 
-        # Process each row in the CSV
-        for i, row in enumerate(csv_reader):
-            if i == 0:
-                continue
-            rows.append(row)
-            
-        # Print the processed rows
-        print("Processed rows:")
+        message_attrs = {'AttributeName': {'StringValue': 'AttributeValue', 'DataType': 'String'}}
         for row in rows:
             print(row)
 
-        # # Send each row as a message to SQS
-        # try:
-        #     message_attrs = {
-        #         'AttributeName': {'StringValue': 'AttributeValue', 'DataType': 'String'}
-        #     }
-        #     for row in rows:
-        #         print(row)
-        #         sqs.send_message(
-        #             QueueUrl=queue_url,
-        #             MessageBody=row[0],
-        #             MessageAttributes=message_attrs,
-        #         )
+            message_body = {
+                "card_number": row[0],
+                "first_name": row[1],
+                "last_name": row[2],
+                "email": row[3],
+                "points": row[4]
+            }
 
-        #     message = 'Message accepted!'
-        # except Exception as e:
-        #     print('Sending message to SQS queue failed!')
-        #     message = str(e)
-        #     status_code = 500
+            try:
+                res = sqs.send_message(
+                    QueueUrl=queue_url,
+                    MessageBody=json.dumps(message_body),
+                    MessageAttributes=message_attrs,
+                )
+                print(res)
+            except Exception as e:
+                print(e)
 
-        # return {'statusCode': status_code, 'body': json.dumps({'message': message})}
+        message = 'Messages accepted!'
+        print(message)
+        response = {"statusCode": 200, "body": json.dumps({"status": "success", "message": message})}
 
     except Exception as e:
-        print(e)
-        print('Error getting object {} from bucket {}. Make sure they exist and your bucket is in the same region as this function.'.format(file_key, bucket))
-        status_code = 500
-        message = str(e)
-        return {'statusCode': status_code, 'body': json.dumps({'message': message})}
+        print(f'Error: {str(e)}')
+        response = {"statusCode": 500, "body": json.dumps({"status": "error", "message": str(e)})}
+
+    return response
+
+def process_sqs_job(event, context):
+    try:
+        print(f"Received SQS event: {json.dumps(event)}")
+
+        table_name = os.getenv("DYNAMODB_CARDS_TABLE_NAME")
+
+        for record in event['Records']:
+            # Parse JSON content from SQS message
+            message_body = json.loads(record['body'])
+
+            if isinstance(message_body, dict):
+                # Extract necessary information from the message
+                card_number = message_body.get('card_number')
+                first_name = message_body.get('first_name')
+                last_name = message_body.get('last_name')
+                email = message_body.get('email')
+                points = message_body.get('points')
+
+                # Check if the email already exists in the DynamoDB table
+                if email_exists(table_name, email):
+                    print(f"Email {email} already used. Skipping...")
+                    continue
+
+                # Create a loyalty card in DynamoDB
+                loyalty_card = {
+                    "card_number": card_number,
+                    "first_name": first_name,
+                    "last_name": last_name,
+                    "email": email,
+                    "points": points
+                }
+
+                DynamodbGateway.upsert(
+                    table_name=table_name,
+                    mapping_data=[loyalty_card],
+                    primary_keys=["card_number"]
+                )
+
+                print(f"Loyalty card created: {loyalty_card}")
+
+        message = 'Messages processed successfully!'
+        print(message)
+        response = {"statusCode": 200, "body": json.dumps({"status": "success", "message": message})}
+
+    except Exception as e:
+        print(f'Error: {str(e)}')
+        response = {"statusCode": 500, "body": json.dumps({"status": "error", "message": str(e)})}
+
+    return response
+
+
+def email_exists(table_name, email):
+    # Check if the email already exists in the DynamoDB table using GSI
+    result = DynamodbGateway.query_index_by_partition_key(
+        index_name="emailIndex",
+        table_name=table_name,
+        partition_key_name="email",
+        partition_key_query_value=email
+    )
+
+    return bool(result)
